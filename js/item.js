0ef484c67c1687332d4bdfa72891f2bdffece39f
Maxi Laya <maxilaya@gmail.com>
2023-11-26 14:44:13 -0300

    se agrega funcionalidad al boton + & -

---
+++ js/item.js
@@ -0,0 +1,6 @@
+const add = document.querySelector('#add');
+const substract = document.querySelector('#substract');
+const quantity =document.querySelector('#quantity');
+
+add.addEventListener('click', () => quantity.value = Number(quantity.value) + 1);
+substract.addEventListener('click', () => quantity.value = Number(quantity.value) - 1);
