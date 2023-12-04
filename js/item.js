7e1f52e83f0ba50ac3d8d2f8eb4547619115562b
Maxi Laya <maxilaya@gmail.com>
2023-12-04 20:30:42 -0300

    mission 3 complete

--- js/item.js
+++ js/item.js
@@ -3,4 +3,7 @@ const substract = document.querySelector('#substract');
 const quantity =document.querySelector('#quantity');
 
 add.addEventListener('click', () => quantity.value = Number(quantity.value) + 1);
-substract.addEventListener('click', () => quantity.value = Number(quantity.value) - 1);
+substract.addEventListener('click', () => {
+    if (quantity.value > 0)
+    quantity.value = Number(quantity.value) - 1
+});
