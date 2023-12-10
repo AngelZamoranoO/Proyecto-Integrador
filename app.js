fe380be73538b1235dcecbd429e14aad0be5e2b5
Luisiana22 <lhaiquettin@gmail.com>
2023-12-10 12:13:37 -0300

    _

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 const express = require("express");
+const app = express();
+const methodOverride = require("method-override");
+
+// ROUTES IMPORTS
 
 const mainRoutes = require("./src/routes/mainRoutes");
 const shopRoutes = require("./src/routes/shopRoutes");
 const adminRoutes = require("./src/routes/adminRoutes");
 const authRoutes = require("./src/routes/authRoutes");
 
-
-// const mainControllers = require("./controllers/mainController");
-
-const app = express();
-
 app.use(express.static("public_html"));
 
-// app.get("/ping", (req, res) => res.send("pong")); EJEMPLO
+app.use(express.urlencoded()); //POST transforme el body en un JS que el prog pueda entender
+app.use(express.json()); //que los formularios entren por PUT
+app.use(methodOverride("_method")); //config luego
 
 app.use("/", mainRoutes);
 app.use("/shop", shopRoutes);
 app.use("/admin", adminRoutes);
 app.use("/auth", authRoutes);
-
-
-// app.use(mainControllers);
 
 app.listen(3000, () =>
   console.log("Servidor corriendo en http://localhost:3000")
