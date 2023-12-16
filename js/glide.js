c68ecac28ab7cb584e74ed36c15d0f2847ab9cf3
Maxi Laya <maxilaya@gmail.com>
2023-12-15 21:50:49 -0300

    feature/responsive-shop

--- js/glide.js
+++ js/glide.js
@@ -1,14 +1,18 @@
-new Glide('.glide', {
-    type: 'carousel',
+
+
+// SLIDER
+
+new Glide(".glide", {
+    type: "carousel",
     startAt: 0,
     perView: 3,
-    gap: 30,
-    breakpoint: {
-        991: {
-            perView: 2
+    gap: 15,
+    breakpoints: {
+        767: {
+            perView: 2,
         },
-        768: {
-            perView: 1
+        425: {
+            perView: 1,
         }
-    }
+    },
 }).mount();
--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,6 @@
 const navbarToggler = document.querySelector(".navbar-toggler");
 const navbarMenu = document.querySelector(".navbar__menu");
+const navbarSubmenu = document.querySelector(".submenu")
 
 navbarToggler.addEventListener("click", () => {
   navbarMenu.classList.toggle("show-menu");
